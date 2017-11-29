55871c3281c179c61a35be027c34478d5a9d3d4a
toran billups <toranb@gmail.com>
2017-11-28 20:42:59 -0600

    FEATURE: extended middleware to support setup

--- src/store.js
+++ src/store.js
@@ -3,11 +3,18 @@ import enhancers from './enhancers/index';
 import middlewares from './middleware/index';
 import { createStore, applyMiddleware, compose } from 'redux';
 
+// Handle "classic" middleware exports (i.e. an array), as well as the hash option
+const extractMiddlewareConfig = (mc) => {
+  return mc.constructor === Array ? { middleware: mc } : mc;
+}
+
 let store;
 const makeStoreInstance = (initState) => {
   if (!store) {
-    let createStoreWithMiddleware = compose(applyMiddleware(...middlewares), enhancers)(createStore);
+    let { middleware, setup = () => {} } = extractMiddlewareConfig(middlewares);
+    let createStoreWithMiddleware = compose(applyMiddleware(...middleware), enhancers)(createStore);
     store = createStoreWithMiddleware(reducers, initState);
+    setup(store);
   }
   return store;
 }
